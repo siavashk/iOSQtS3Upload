73a2d88c54c9525f1aa2e2478fd852a6ed6c5a59
Siavash Khallaghi <siavashk@ece.ubc.ca>
2017-10-19 14:59:10 -0700

    Minimal upload example. Should start cleaning now.

--- main.m
+++ main.m
@@ -1,48 +1,68 @@
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-
-NSURLSessionUploadTask* createUploadTask(NSString* filename)
+void printFileContents(NSString* filePath)
 {
-    NSURLSessionConfiguration* configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-    NSURLSession* session = [NSURLSession sessionWithConfiguration:configuration delegate:nil delegateQueue:nil];
+    NSError *error;
+    NSString *fileContents = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:&error];
     
+    if (error)
+        NSLog(@"Error reading file: %@", error.localizedDescription);
+    
+    NSLog(@"contents: %@", fileContents);
+    
+    NSArray *listArray = [fileContents componentsSeparatedByString:@"\n"];
+    NSLog(@"items = %luld", (unsigned long)[listArray count]);
+}
+
+@protocol UploadProtocol
+- (void)uploadFile:(NSURLSession*)session filePath:(NSString*)filePat completionHandler:(void(^)(NSData *data, NSURLResponse *response, NSError *error))completionHandler;
+@end
+
+@interface S3 : NSObject<UploadProtocol>
+{}
+@end
+
+@implementation S3
+
+- (void)uploadFile:(NSURLSession*)session filePath:(NSString*)filePath completionHandler:(void(^)(NSData *data, NSURLResponse *response, NSError *error))completionHandler
+{
     NSURL* url = [NSURL URLWithString:@"https://s3.ca-central-1.amazonaws.com/stage-ca"];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
     
     NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"];
     [request setValue:contentType forHTTPHeaderField: @"Content-Type"];
     
-    return [session uploadTaskWithRequest:request fromFile:[NSURL fileURLWithPath:filename]
-        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
-            {
-                if (error)
-                {
-                    NSLog(@"error = %@", error);
-                    return;
-                }
-                
-                NSString *result = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-                NSLog(@"result = %@", result);
-            }];
+    NSURLSessionUploadTask* task = [session uploadTaskWithRequest:request fromFile:[NSURL fileURLWithPath:filePath] completionHandler:completionHandler];
+    [task resume];
 }
+
+@end
 
 int main(int argc, const char * argv[])
 {
+    NSURLSessionConfiguration* configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession* session = [NSURLSession sessionWithConfiguration:configuration delegate:nil delegateQueue:nil];
     
     NSString* filePath = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"txt"];
-
-    // Debug code
-//    NSError *error;
-//    NSString *fileContents = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:&error];
-//
-//    if (error)
-//        NSLog(@"Error reading file: %@", error.localizedDescription);
-//
-//    // maybe for debugging...
-//    NSLog(@"contents: %@", fileContents);
-//
-//    NSArray *listArray = [fileContents componentsSeparatedByString:@"\n"];
-//    NSLog(@"items = %d", [listArray count]);
-     NSURLSessionUploadTask* task = createUploadTask(filePath);
-     [task resume];
+    
+    dispatch_group_t group = dispatch_group_create();
+    void (^completionHandler)(NSData *data, NSURLResponse *response, NSError *error) = ^(NSData *data, NSURLResponse *response, NSError *error)
+    {
+        if (error)
+        {
+            NSLog(@"error = %@", error);
+            return;
+        }
+        
+        NSString *result = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"result = %@", result);
+        dispatch_group_leave(group);
+    };
+    
+    S3* s3 = [[S3 alloc] init];
+    
+    dispatch_group_enter(group);
+    [s3 uploadFile:session filePath:filePath completionHandler:completionHandler];
+    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
 }
